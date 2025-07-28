cddf2cd9438924e6c52186f6c15860829e0c8bac
max <www.max.021persia@gmail.com>
2025-07-28 22:12:24 +0330

    dashboard bufigfix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Post ;
+
 class DashboardController extends Controller
 {
     public function index () {
-        $posts = $post->latest()->paginate(6) ; 
+        $posts = Auth::user()->posts()->latest()->paginate(6) ; 
         return view('user.dashboard' , ['posts' => $posts]) ; 
     }
 
