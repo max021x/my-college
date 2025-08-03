64ad08a57b3a168b02b52c9490c1f354cc1733a2
max <www.max.021persia@gmail.com>
2025-08-03 19:53:40 +0330

    login page added css fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,31 +3,38 @@
         <p>{{ session('status') }}</p>
     @endif
 
-    <section>
-        <h1>Welcome Back Soldier</h1>
-        <form action="{{ route('auth.login') }}" method="post">
-            @csrf
-            <div>
-                <label for="email">Eamil</label>
-                <input type="email" name="email" value="{{ old('email') }}">
-            </div>
+    <div class="container">
+        <section class="form__container">
+            <h1>Welcome Back Soldier</h1>
+            <form action="{{ route('auth.login') }}" method="post">
+                @csrf
+                <div class="input">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" value="{{ old('email') }}"
+                    >
+                </div>
 
-            <div>
-                <label for="password">Password</label>
-                <input type="password" name="password">
-            </div>
-            <div>
-                <input type="checkbox" name="remember">
-                <span>Remember Me</span>
+                <div class="input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                </div>
+                <div class="input box">
+                    <div class="checkbox">
+                        <span>Remember Me</span>
+                        <input type="checkbox" name="remember">
+                    </div>
 
-                <a class="text-blue-500" href="{{ route('password.request') }}">Forgot your password?</a>
-            </div>
+                    <a class="text-blue-500" href="{{ route('password.request') }}">Forgot your password?</a>
+                </div>
 
-            @error('message')
-                <p class="error">{{ $message }}</p>
-            @enderror
+                @error('message')
+                    <p class="error">{{ $message }}</p>
+                @enderror
 
-            <button type="submit">Submit</button>
-        </form>
-    </section>
+                <button
+                    class="btn px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+                    type="submit">Submit</button>
+            </form>
+        </section>
+    </div>
 </x-layout>
