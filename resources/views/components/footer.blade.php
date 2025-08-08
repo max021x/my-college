d73538b18f2ecb4ac74068df009ce3907d9d1c03
max <www.max.021persia@gmail.com>
2025-08-08 11:40:15 +0330

    links set

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,7 +9,7 @@
 
             <div class="flex -mx-1">
                 <!-- Telegram -->
-                <a href="#" class="mx-1 text-gray-600 hover:text-blue-500" aria-label="Telegram">
+                <a href="https://t.me/mycollegee" class="mx-1 text-gray-600 hover:text-blue-500" aria-label="Telegram">
                     <svg class="w-6 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.57-1.38-.93-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69.03-.1.05-.2-.04-.3-.1-.1-.25-.07-.36-.04-.15.05-2.57 1.66-7.27 4.88-.7.5-1.34.74-1.91.73-.6-.01-1.76-.34-2.62-.63-1.07-.36-1.92-.55-1.85-1.16.03-.3.45-.61 1.24-.88z" />
@@ -17,7 +17,7 @@
                 </a>
 
                 <!-- Gmail -->
-                <a href="#" class="mx-1 text-gray-600 hover:text-red-500" aria-label="Gmail">
+                <a href="mailto:max.021persia@gmail.com" class="mx-1 text-gray-600 hover:text-red-500" aria-label="Gmail">
                     <svg class="w-6 h-5" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
